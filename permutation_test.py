87b1090d7e0d7ed5e28ef3570078ebb542b999b7
Gustavo Jeuken <gjeuken@gmail.com>
2021-03-19 14:38:57 +0100

    Permutation code

---
+++ permutation_test.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import sklearn as skl
+import porch
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn  import preprocessing
+from sklearn.metrics import pairwise_distances
+import pickle
+import lifelines
+import qvalue
+from joblib import Parallel, delayed
+import multiprocessing
+
+
+from run_analysis import load_metabric, illumina2ensembl_dictionary, get_reactome_illumina, return_pathway
+
+
+def permute_pathway(activity):
+    shuffle.columns = np.random.permutation(metadata_df.columns) 
+    rowresult = porch.survival(activity, shuffle, duration_col = 'T', event_col = 'E')
+    return rowresult['coef']
+
+def parallel_permutations(activity_row, metadata_df, n, num_cores = 7):
+    activity = activity_row[:-2]
+    results = Parallel(n_jobs = num_cores)(delayed(permute_pathway)(activity) for i in range(n))
+    return results
+
+
+if __name__ == "__main__":
+
+    ## Load data
+
+    metabric_path = '../../data/metabric'
+    illumina2ensembl_path = '../../data/reactome/illumina2ensembl.txt'
+
+    data = load_metabric(metabric_path)
+    expression_df = data.iloc[8:,:]
+    metadata_df = data.iloc[:8,:]
+    # reactome_df = get_reactome_illumina(illumina2ensembl_path)
+    activity_df = pickle.load(open('metabric_path_activities.p', 'rb'))
+    activity_df.index = [x.replace('-','_') for x in activity_df.index]
+    survival = pickle.load(open('metabric_path_survival.p', 'rb'))
+
+    metadata_df.loc['E'] = (metadata_df.loc['last_follow_up_status'] == 'd-d.s.')*1
+    shuffle = metadata_df.loc[['E','T']].copy()
+
+    num_cores = multiprocessing.cpu_count() - 1
+
+    n_tests = 100
+    p_min = -5
+    # p_list = np.arange(0,p_min,p_min/n_tests)
+    p_list = np.random.uniform(p_min,0,n_tests)
+    p_list = np.sort(10**p_list)[::-1]
+
+    permutation_results = pd.DataFrame(columns=['base','perms'])
+
+    for p_val in p_list:
+        nearest = np.abs(survival['p'] - p_val).sort_values().index[0]
+        activity_row = activity_df.loc[nearest]
+
+        n_pemutation = np.ceil((1/survival.loc[nearest, 'p']) * 10).astype(int)
+
+        print('Permutation test for ' + str(nearest) + ', number of permutations:' + str(n_pemutation))
+        perm_results = parallel_permutations(activity_row, metadata_df, n_pemutation, num_cores=num_cores)
+
+        permutation_results.loc[nearest, 'base'] = survival.loc[nearest,'coef']
+        permutation_results.loc[nearest, 'perms'] = perm_results
+
+        pickle.dump(permutation_results, open("permutation_results.p", "wb"))
---
+++ set_permutation.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import pickle
+import multiprocessing
+from joblib import Parallel, delayed
+
+from run_analysis import load_metabric, illumina2ensembl_dictionary, get_reactome_illumina, return_pathway
+import porch
+
+def permute_set(set_size):
+    try:
+        set_genes = np.random.choice(genes, set_size)
+        setname, set_annot, set_size, activity, eigen_sample_dict  = porch.porch_proc('setname', 'annot', set_genes, expression_df)
+        activity_df = pd.DataFrame(data=activity, index=expression_df.columns, columns= ['set' + str(set_size)]).T
+        rowresult = porch.survival(activity_df.iloc[0], metadata_df, duration_col = 'T', event_col = 'E')
+        return rowresult['z']
+    except:
+        return 'error'
+
+
+if __name__ == "__main__":
+    metabric_path = '../../data/metabric'
+    illumina2ensembl_path = '../../data/reactome/illumina2ensembl.txt'
+
+    data = load_metabric(metabric_path)
+    expression_df = data.iloc[8:,:]
+    metadata_df = data.iloc[:8,:]
+
+    metadata_df.loc['E'] = (metadata_df.loc['last_follow_up_status'] == 'd-d.s.')*1
+
+    genes = expression_df.index
+
+    activities = pickle.load(open('metabric_path_activities.p', 'rb'))
+    set_sizes = np.unique(activities['set_size'])
+
+    num_cores = multiprocessing.cpu_count() - 1
+    num_perm = 10**4
+
+    results = pd.DataFrame(columns=np.arange(num_perm))
+    
+    i = 0
+    for set_size in set_sizes:
+        i+=1
+        print('Permutations for set size ' + str(set_size) + ', ' + str(i) + '/' + str(len(set_sizes)))
+        permutaions = Parallel(n_jobs = num_cores, prefer="threads")(delayed(permute_set)(set_size) for i in range(num_perm))
+        results.loc[set_size] = permutaions 
+        pickle.dump(results, open('set_permutation_results.p', 'wb'))
+
+###
+    
+    survival = pickle.load(open('metabric_path_survival.p', 'rb'))
+    survival.index = [x.replace('_','-') for x in survival.index]
+    survival['ngenes'] = activity['set_size']
+
+
+    for pathway, row in survival.iterrows():
+        z = np.abs(row['z'])
+        perms = perm_results.loc[row['ngenes']]
+        perms = np.abs(perms[perms != 'error'])
+        num_higher = sum(x > z for x in perms)
+        p = num_higher/len(perms)
+        survival.loc[pathway, 'p_perms'] = p
+
+    survival['p_perms'] = np.round(survival['p_perms'], decimals=4)
+    survival['annotation'] = activity['annotation'] 
+    survival[['annotation', 'p_perms']].sort_values('p_perms').to_csv('permutation_p_results.csv')
+
+
