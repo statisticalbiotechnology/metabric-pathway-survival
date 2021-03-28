846015ee71575d2b927ef7684328e7b126709083
Gustavo Jeuken <gjeuken@gmail.com>
2021-03-29 00:26:59 +0200

    comment out unused plots

--- plots.py
+++ plots.py
@@ -104,84 +104,84 @@ if __name__ == "__main__":
 
     ## In here we try a new kind of plot to show survival and gene correlation
 
-    metadata = metadata_df
-    reactome_df = illumina_reactome_df
-    reactome_id = 'R-HSA-196757' 
+    # metadata = metadata_df
+    # reactome_df = illumina_reactome_df
+    # reactome_id = 'R-HSA-196757' 
 
-    pathway_data = return_pathway(data, reactome_df, reactome_id)
-    vec, exp = return_PC1(pathway_data)
-    sort_df = pd.DataFrame(vec, index=pathway_data.columns)
-    sort_seq = sort_df.sort_values(0).index.tolist()
+    # pathway_data = return_pathway(data, reactome_df, reactome_id)
+    # vec, exp = return_PC1(pathway_data)
+    # sort_df = pd.DataFrame(vec, index=pathway_data.columns)
+    # sort_seq = sort_df.sort_values(0).index.tolist()
 
-    x = preprocessing.StandardScaler().fit_transform(pathway_data)
-    pathway_data.loc[:,:] = x
-    genes = pathway_data.columns
-    pathway_data['T'] = metadata.T['T']
-    pathway_data['E'] = (metadata.T['last_follow_up_status'] != 'a')*1
+    # x = preprocessing.StandardScaler().fit_transform(pathway_data)
+    # pathway_data.loc[:,:] = x
+    # genes = pathway_data.columns
+    # pathway_data['T'] = metadata.T['T']
+    # pathway_data['E'] = (metadata.T['last_follow_up_status'] != 'a')*1
 
-    pathway_data = pathway_data.where(pathway_data['E'] == 1).dropna()
+    # pathway_data = pathway_data.where(pathway_data['E'] == 1).dropna()
 
-    pathway_data_quantiles = pathway_data.groupby(pd.cut(pathway_data['T'], pathway_data['T'].quantile(np.arange(0.1,1,0.1)))).mean()
+    # pathway_data_quantiles = pathway_data.groupby(pd.cut(pathway_data['T'], pathway_data['T'].quantile(np.arange(0.1,1,0.1)))).mean()
 
-    pathway_data_quantiles['id'] = ['(' + str(np.round(x.left/365, 1)) + ', ' + str(np.round(x.right/365, 1)) + ']' for x in pathway_data_quantiles.index]
+    # pathway_data_quantiles['id'] = ['(' + str(np.round(x.left/365, 1)) + ', ' + str(np.round(x.right/365, 1)) + ']' for x in pathway_data_quantiles.index]
 
-    pathway_data_long = pd.wide_to_long(pathway_data_quantiles, stubnames='ILMN', sep = '_', i='id', j='probe')
-    pathway_data_long = pathway_data_long.reset_index(level=1)
-    pathway_data_long['T'] = [np.round(x.mid) for x in pathway_data_long.index]
-    pathway_data_long['T'] = pathway_data_long.index
+    # pathway_data_long = pd.wide_to_long(pathway_data_quantiles, stubnames='ILMN', sep = '_', i='id', j='probe')
+    # pathway_data_long = pathway_data_long.reset_index(level=1)
+    # pathway_data_long['T'] = [np.round(x.mid) for x in pathway_data_long.index]
+    # pathway_data_long['T'] = pathway_data_long.index
 
 
-    order = [int(x[5:]) for x in sort_seq]
-    name = reactome_df.where(reactome_df['reactome_id'] == reactome_id).dropna()['reactome_name'].iloc[0]
+    # order = [int(x[5:]) for x in sort_seq]
+    # name = reactome_df.where(reactome_df['reactome_id'] == reactome_id).dropna()['reactome_name'].iloc[0]
 
-    chart = sns.pointplot(x = 'probe', y = 'ILMN', hue = 'T', data=pathway_data_long, order = order, palette='vlag', scale = 0.5, errwidth=0)
-    plt.legend(bbox_to_anchor=(1, 1), title = 'Survival time (years)')
-    plt.ylabel('Mean normalized expression')
-    plt.xlabel('Gene')
-    chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
-    plt.plot(sort_df.sort_values(0).values, color = 'y', linestyle = '--', linewidth = 2)
-    plt.title(name)
+    # chart = sns.pointplot(x = 'probe', y = 'ILMN', hue = 'T', data=pathway_data_long, order = order, palette='vlag', scale = 0.5, errwidth=0)
+    # plt.legend(bbox_to_anchor=(1, 1), title = 'Survival time (years)')
+    # plt.ylabel('Mean normalized expression')
+    # plt.xlabel('Gene')
+    # chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
+    # plt.plot(sort_df.sort_values(0).values, color = 'y', linestyle = '--', linewidth = 2)
+    # plt.title(name)
 
-    ax = plt.gca()
-    labels = ['ILMN_' + x.get_text() for x in ax.get_xticklabels()]
-    dictionary_pd = illumina2ensembl_dictionary('data/illumina2hugo.txt').set_index('probe')
-    labels_gene = [np.unique(dictionary_pd.loc[x,'gene'])[0] for x in labels]
+    # ax = plt.gca()
+    # labels = ['ILMN_' + x.get_text() for x in ax.get_xticklabels()]
+    # dictionary_pd = illumina2ensembl_dictionary('data/illumina2hugo.txt').set_index('probe')
+    # labels_gene = [np.unique(dictionary_pd.loc[x,'gene'])[0] for x in labels]
 
-    chart.set_xticklabels(labels_gene)
+    # chart.set_xticklabels(labels_gene)
 
-    plt.tight_layout()
-    plt.savefig('plots/survial_eigenpatient.png', bbox_inches='tight') 
+    # plt.tight_layout()
+    # plt.savefig('plots/survial_eigenpatient.png', bbox_inches='tight') 
 
     #### We compare patways against their most predictive gene
 
-    genes_cox['q'] = qvalue.qvalues(genes_cox)
+    # genes_cox['q'] = qvalue.qvalues(genes_cox)
 
-    for path in survival.index:
-        print('Compare' + path)
-        genes = return_pathway(genes_cox,illumina_reactome_df,path).T
-        max_p = genes.sort_values('p').iloc[0]['p']
-        max_q = genes.sort_values('q').iloc[0]['q']
-        ngenes = genes.shape[0]
-        survival.loc[path,'max_probe_p'] = max_p
-        survival.loc[path,'max_probe_q'] = max_q
-        survival.loc[path,'ngenes'] = ngenes
+    # for path in survival.index:
+    #     print('Compare' + path)
+    #     genes = return_pathway(genes_cox,illumina_reactome_df,path).T
+    #     max_p = genes.sort_values('p').iloc[0]['p']
+    #     max_q = genes.sort_values('q').iloc[0]['q']
+    #     ngenes = genes.shape[0]
+    #     survival.loc[path,'max_probe_p'] = max_p
+    #     survival.loc[path,'max_probe_q'] = max_q
+    #     survival.loc[path,'ngenes'] = ngenes
 
-    survival['q'] = qvalue.qvalues(survival, pcolname='p')
+    # survival['q'] = qvalue.qvalues(survival, pcolname='p')
 
-    plot = sns.scatterplot(x = survival['p'],
-            y = survival['max_probe_p'],
-            alpha = 0.5)
-    plot.set(xscale = 'log', yscale = 'log', xlabel = 'Pathway p value', ylabel = 'Minimum probe p value')
-    plt.plot([1e-24,1], [1e-24,1], linewidth=1, color = 'r')
-    plt.savefig('plots/pathway_gene_p.png')
+    # plot = sns.scatterplot(x = survival['p'],
+    #         y = survival['max_probe_p'],
+    #         alpha = 0.5)
+    # plot.set(xscale = 'log', yscale = 'log', xlabel = 'Pathway p value', ylabel = 'Minimum probe p value')
+    # plt.plot([1e-24,1], [1e-24,1], linewidth=1, color = 'r')
+    # plt.savefig('plots/pathway_gene_p.png')
 
 
-    plot = sns.scatterplot(x = survival['q'],
-            y = survival['max_probe_q'],
-            alpha = 0.5)
-    plot.set(xscale = 'log', yscale = 'log', xlabel = 'Pathway $q$ value', ylabel = 'Minimum probe $q$ value')
-    plt.plot([1e-21,1], [1e-21,1], linewidth=1, color = 'r')
-    plt.savefig('plots/pathway_gene_q.png', bbox_inches='tight')
+    # plot = sns.scatterplot(x = survival['q'],
+    #         y = survival['max_probe_q'],
+    #         alpha = 0.5)
+    # plot.set(xscale = 'log', yscale = 'log', xlabel = 'Pathway $q$ value', ylabel = 'Minimum probe $q$ value')
+    # plt.plot([1e-21,1], [1e-21,1], linewidth=1, color = 'r')
+    # plt.savefig('plots/pathway_gene_q.png', bbox_inches='tight')
 
 
     ### compare concordance indexes
@@ -231,7 +231,7 @@ if __name__ == "__main__":
 
     pathway_data_dead.sort_values(inplace=True,by=['T'])
     pathway_data_dead = pathway_data_dead[['Eigengene','T']+sort_seq]
-    dictionary_pd = illumina2ensembl_dictionary('../../data/reactome/illumina2hugo.txt'
+    dictionary_pd = illumina2ensembl_dictionary('data/illumina2hugo.txt'
             ).set_index('probe')
     labels_gene = [np.unique(dictionary_pd.loc[x,'gene'])[0] for x in pathway_data_dead.columns if 'ILMN_' in x]
     pathway_data_dead.columns = [ x for x in pathway_data_dead.columns if 'ILMN_' not in x ] + labels_gene
@@ -263,23 +263,23 @@ if __name__ == "__main__":
 
     #### Permutation test
 
-    permutation_results = pickle.load(open('results/permutation_results.p', 'rb'))
-    cox_results = pickle.load(open('results/metabric_path_survival.p', 'rb'))
+    # permutation_results = pickle.load(open('results/permutation_results.p', 'rb'))
+    # cox_results = pickle.load(open('results/metabric_path_survival.p', 'rb'))
 
-    for pathway, row in permutation_results.iterrows():
-        z = np.abs(row['base'])
-        perms = np.abs(row['perms'])
-        num_higher = sum(x >= z for x in perms)
-        p = num_higher/len(perms)
-        cox_results.loc[pathway,'p_perms'] = p
+    # for pathway, row in permutation_results.iterrows():
+    #     z = np.abs(row['base'])
+    #     perms = np.abs(row['perms'])
+    #     num_higher = sum(x >= z for x in perms)
+    #     p = num_higher/len(perms)
+    #     cox_results.loc[pathway,'p_perms'] = p
 
-    cox_results = cox_results.dropna()
+    # cox_results = cox_results.dropna()
 
-    plt.scatter(cox_results['p'],cox_results['p_perms'], alpha = 0.5)
-    plt.xlabel('Cox derived p value')
-    plt.ylabel('Permutation derived p value')
-    plt.yscale('log')
-    plt.xscale('log')
-    lim = np.min([np.min(cox_results['p']), np.min(cox_results['p_perms'])])
-    plt.plot([1,lim],[1,lim], color='r', alpha = 0.5)
-    plt.savefig('plots/permutation.png, bbox_inches='tight'')
+    # plt.scatter(cox_results['p'],cox_results['p_perms'], alpha = 0.5)
+    # plt.xlabel('Cox derived p value')
+    # plt.ylabel('Permutation derived p value')
+    # plt.yscale('log')
+    # plt.xscale('log')
+    # lim = np.min([np.min(cox_results['p']), np.min(cox_results['p_perms'])])
+    # plt.plot([1,lim],[1,lim], color='r', alpha = 0.5)
+    # plt.savefig('plots/permutation.png, bbox_inches='tight'')
